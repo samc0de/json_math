8f6512723b72099cec95a9bc71155872248d16cc
Sameer <sssam.sam@gmail.com>
2018-05-27 15:08:18 +0530

    Add a _are_all_compatible.
    Add a test for _are_all_compatible.
    Add the function itself.
    Make it pass the test. Also there are other tests added/modified, which still fail.

--- add.py
+++ add.py
@@ -2,8 +2,53 @@
 
 This should add keys of multiple json objects, add values if the keys are same,
 and add recursively, probably controlled by a flag, defaulting to True."""
+import json
+import logging
+
+
+class TypeIncompatibilityError(TypeError):
+  """Raised when types of objects are not compatible."""
+  message = 'Not all objects are compatible with each other.'
+  def __init__(self, msg='', value=None, *args):
+    msg = msg or self.message
+    if value:
+      args = args + (value,)
+    return super(TypeIncompatibilityError, self).__init__(msg, *args)
+
+
+# Add some function here to determine diff type compatibility here. Should be
+# compatible with all().
+
+
+def _are_all_compatible(*objects):
+  """Tell if all objects are compatible for math operations."""
+  # Start with simple soution, the types being same.
+  base_type = None
+  for obj in objects:
+    obj_type = type(obj)
+    if base_type is None:
+      base_type = obj_type
+    elif base_type != obj_type:
+      return False
+  return True
+  # # Another approach.
+  # first = objects[0]
+  # start = type(first)()  # base_additive
+  # try:
+  #   _ = sum(objects, start)
+  # except TypeError as e:
+  #   _, value, traceback = sys.exc_info()
+  #   # Value can be sent as 2nd expression, with 1st just a class, not an obj.
+  #   logging.error(str(e))
+  #   # Do not raise, just log it.
+  #   raise TypeIncompatibilityError(value=value), None, traceback
+
+
 
 
 def add(*args):
   """Add json/dict objects."""
-  pass
+  # Provide way of passing options to json.loads via kwargs.
+  objects = map(json.loads, args)
+  if not _are_all_compatible(objects):
+    raise TypeIncompatibilityError()
--- tests/test_add.py
+++ tests/test_add.py
@@ -1,24 +1,40 @@
+import json
 import unittest
 
+from context import add
 
-class AddTest(unittest.TestCase):
-  """Tests add function of add.py."""
-  def test_add(self):
-    """Test basic addition."""
-    json_1 = {
-        'foo': 1,
-        'bar': 'a'
-        }
-    json_2 = {
-        'baz': 'baz'
-        }
-    actual = add.add(json_1, json_2)
-    expected = {
-        'foo': 1,
-        'bar': 'a',
-        'baz': 'baz'
-        }
-    self.assertEqual(expected, actual)
+
+# class AddTest(unittest.TestCase):
+#   """Tests add function of add.py."""
+#   def test_asserts_type_equality(self):
+#     """Tests whether add() checks type compatibility."""
+#     a_list = range(4)
+#     a_dict = dict(enumerate('abcd'))
+#     self.assertRaises(add.TypeIncompatibilityError, add.add, a_list, a_dict)
+# 
+#   def test_add(self):
+#     """Test basic addition."""
+#     json_1 = {
+#         'foo': 1,
+#         'bar': 'a'
+#         }
+#     json_2 = {
+#         'baz': 'baz'
+#         }
+#     actual = add.add(json_1, json_2)
+#     expected = {
+#         'foo': 1,
+#         'bar': 'a',
+#         'baz': 'baz'
+#         }
+#     self.assertEqual(expected, actual)
+
+
+class AreAllCompatibleTest(unittest.TestCase):
+  def test_all_same_type_list(self):
+    """Test behaviour when all objects are of same type."""
+    objects = map(json.dumps, (['abc', 2, 'a'], range(4), ['foo', 'bar']))
+    self.assertTrue(add._are_all_compatible(objects))
 
 
 if __name__ == '__main__':
